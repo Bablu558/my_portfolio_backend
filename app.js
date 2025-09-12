9739ed12f56cf5a5c12ba88e72712b49d3715bd8
Git-Github <kumarbablu03240@gmail.com>
2025-09-12 15:45:38 +0530

    latest commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
     cors({
 
       // origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
-      origin: ["http://localhost:5173", "http://localhost:5174"],
+      origin: ["http://localhost:5173", "http://localhost:5174", "https://bablu-portfolio-dashboard.netlify.app"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
