135ed5acd00ae173c2e0e1210478bc8a3bace459
Git-Github <kumarbablu03240@gmail.com>
2025-10-12 21:19:14 +0530

    Auth

--- router/blogRoute.js
+++ router/blogRoute.js
@@ -6,17 +6,17 @@ import {
   deleteBlog,
   updateBlog,
 } from "../controller/blogController.js";
-// import { isAuthenticated } from "../middleware/auth.js";
+import { isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Public routes
-router.get("/getall", getAllBlogs);
-router.get("/get/:id", getBlogById);
+router.get("/getall",isAuthenticated, getAllBlogs);
+router.get("/get/:id",isAuthenticated, getBlogById);
 
 // Admin routes
-router.post("/create",  createBlog);
-router.delete("/delete/:id",deleteBlog);
-router.put("/update/:id",updateBlog);
+router.post("/create",isAuthenticated,  createBlog);
+router.delete("/delete/:id",isAuthenticated, deleteBlog);
+router.put("/update/:id",isAuthenticated, updateBlog);
 
 export default router;
