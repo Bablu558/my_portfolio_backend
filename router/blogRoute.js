a4f312e98d4732389fb57770a0962fd08c22cc12
Git-Github <kumarbablu03240@gmail.com>
2025-12-09 16:58:17 +0530

    remove some files

--- router/blogRoute.js
+++ router/blogRoute.js
@@ -11,8 +11,7 @@ import {
 } from "../controller/blogController.js";
 import { isAuthenticated } from "../middleware/auth.js";
 import { isBlogUserAuthenticated } from "../middleware/blogAuth.js";
-import { generateTitleAI } from "../controller/aiController.js";
-import { generateShortDescAI } from "../controller/aiController.js";
+
 const router = express.Router();
 
 // Public routes
@@ -33,6 +32,5 @@ router.delete("/user/delete/:id", isBlogUserAuthenticated, deleteBlog);
 router.get("/user/myblogs", isBlogUserAuthenticated, getMyBlogs);
 router.put("/like/:id", toggleLikeBlog);
 router.get("/get/slug/:slug", getBlogBySlug);
-router.post("/ai/generate-title", generateTitleAI);
-router.post("/ai/generate-summary", generateShortDescAI);
+
 export default router;
