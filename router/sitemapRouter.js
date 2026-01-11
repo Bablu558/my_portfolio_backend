da09f7f70ff2d18fe9d8c4ffe99459a8cc6fb1bc
Git-Github <kumarbablu03240@gmail.com>
2026-01-11 22:01:16 +0530

    new url

--- router/sitemapRouter.js
+++ router/sitemapRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/sitemap.xml", async (req, res) => {
   try {
-    const FRONTEND_URL = "https://blogs.bablukumar.online";
+    const FRONTEND_URL = "https://blogfire.in";
     const BACKEND_API = "https://my-portfolio-backend-5p6u.onrender.com/api/v1/blog/getall";
 
     // Fetch all blogs 
